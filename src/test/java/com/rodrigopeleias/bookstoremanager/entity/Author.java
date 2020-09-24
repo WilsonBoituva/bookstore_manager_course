5f7e91a087af87ccb22a9f12c677813d5259a2a8
WilsonBoituva <inventory_count@outlook.com>
2020-09-24 14:04:34 -0300

    Developed project entities

---
+++ src/test/java/com/rodrigopeleias/bookstoremanager/entity/Author.java
@@ -0,0 +1,29 @@
+package com.rodrigopeleias.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.test.autoconfigure.data.ldap.AutoConfigureDataLdap;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+
+}
