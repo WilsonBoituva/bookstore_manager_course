4f63fa5c0fc6f0e4c2250da3bcb14d1053405399
WilsonBoituva <inventory_count@outlook.com>
2020-09-26 23:26:50 -0300

    Refactores functionality for book creation

--- src/main/java/com/rodrigopeleias/bookstoremanager/entity/Author.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entity/Author.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.test.autoconfigure.data.ldap.AutoConfigureDataLdap;
 
 import javax.persistence.*;
 
