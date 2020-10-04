ce736afe6dd18113bd7f72fa11490cb4f0cf0365
WilsonBoituva <inventory_count@outlook.com>
2020-10-03 21:44:59 -0300

    Developed extension for get by id method

--- src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
@@ -3,6 +3,7 @@ package com.rodrigopeleias.bookstoremanager.controller;
 import com.rodrigopeleias.bookstoremanager.dto.BookDTO;
 import com.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
 import com.rodrigopeleias.bookstoremanager.entity.Book;
+import com.rodrigopeleias.bookstoremanager.exception.BookNotFoundException;
 import com.rodrigopeleias.bookstoremanager.repository.BookRepository;
 import com.rodrigopeleias.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,7 @@ public class BookController {
     }
 
     @GetMapping("/{id}")
-    public BookDTO findById(@PathVariable long id){
+    public BookDTO findById(@PathVariable long id) throws BookNotFoundException {
 
         return  bookService.findById(id);
     }
--- src/main/java/com/rodrigopeleias/bookstoremanager/service/BookService.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/service/BookService.java
@@ -3,6 +3,7 @@ package com.rodrigopeleias.bookstoremanager.service;
 import com.rodrigopeleias.bookstoremanager.dto.BookDTO;
 import com.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
 import com.rodrigopeleias.bookstoremanager.entity.Book;
+import com.rodrigopeleias.bookstoremanager.exception.BookNotFoundException;
 import com.rodrigopeleias.bookstoremanager.mapper.BookMapper;
 import com.rodrigopeleias.bookstoremanager.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,9 +35,11 @@ public class BookService {
 
     }
 
-    public BookDTO findById(long id) {
-        Optional<Book> book = bookRepository.findById(id);
-       return bookMapper.toDTO(book.get());
+    public BookDTO findById(Long id) throws BookNotFoundException {
+        Book book = bookRepository.findById(id)
+                .orElseThrow(() -> new BookNotFoundException(id));
+
+        return bookMapper.toDTO(book);
 
     }
 
